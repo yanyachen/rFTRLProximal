18bbc4251ccb0358620b3760fd6d588acd6cc010
yanyachen <yanyachen@users.noreply.github.com>
2016-09-15 09:30:17 -0700

    More Accurate Reference
    More Accurate Reference

--- R/rFTRLProximal.R
+++ R/rFTRLProximal.R
@@ -1,8 +1,8 @@
 #' rFTRLProximal: FTRL-Proximal Algorithm
 #'
 #' An efficient C++ based implementation of "Follow The (Proximally) Regularized Leader"
-#' online learning algorithm. This algorithm was proposed in McMahan et al. (2013).
-#' (Please see <DOI:10.1145/2487575.2488200> for more information).
+#' online learning algorithm. This algorithm was proposed in McMahan et al. (2013)
+#' <DOI:10.1145/2487575.2488200>.
 #'
 #' @docType package
 #' @name rFTRLProximal
