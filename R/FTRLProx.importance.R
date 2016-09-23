b3eb5e4025fb7719fd27adeecd0f1e54d5d0ddc7
yanyachen <yanyachen@users.noreply.github.com>
2016-09-23 00:19:34 -0700

    Update FTRLProx.importance
    Update FTRLProx.importance

--- R/FTRLProx.importance.R
+++ R/FTRLProx.importance.R
@@ -10,11 +10,11 @@
 #' library(FeatureHashing)
 #' data(ipinyou)
 #' m.train <- FTRLProx_Hashing(~ 0 + ., ipinyou.train[, -"IsClick", with = FALSE],
-#'                             hash.size = 2^10, signed.hash = FALSE, verbose = TRUE)
+#'                             hash.size = 2^13, signed.hash = FALSE, verbose = TRUE)
 #' ftrl_model <- FTRLProx_train(m.train, y = as.numeric(ipinyou.train$IsClick),
 #'                              family = "binomial",
 #'                              params = list(alpha = 0.01, beta = 0.1, l1 = 1.0, l2 = 1.0),
-#'                              epoch = 5, verbose = TRUE)
+#'                              epoch = 10, verbose = TRUE)
 #' FTRLProx.importance(ftrl_model)
 #' @importFrom stats na.omit
 #' @importFrom magrittr %>% %T>%
@@ -24,21 +24,16 @@
 FTRLProx.importance <- function(model) {
   # Solve Hashing Collision
   Mapping_DT_Gen <- function(Mapping) {
-    Mapping_DT <- data.table::data.table(Index = unique(Mapping),
-                                         Feature_Name = character(1))
-    Mapping_Name <- names(Mapping)
-    for (i in seq_len(nrow(Mapping_DT))) {
-      data.table::set(Mapping_DT, i, "Feature_Name",
-                      value = paste(Mapping_Name[Mapping == Mapping_DT[i, Index]], collapse = " + "))
-    }
-    return(Mapping_DT)
+    data.table::data.table(Index = Mapping,
+                           Feature = names(Mapping)) %>%
+      magrittr::extract(., j = .(Feature = paste(get("Feature"), collapse = "+")), by = "Index")
   }
   Mapping_DT <- Mapping_DT_Gen(model$mapping)
   # Feature Importance Generation
-  data.table::data.table(ID = seq_along(model$weight),
+  data.table::data.table(Index = seq_along(model$weight),
                          Weight = model$weight) %T>%
-    magrittr::extract(., j = "Feature" := match(get("ID"), Mapping_DT$Index) %>%
-                        magrittr::extract(Mapping_DT$Feature_Name, .), with = FALSE) %T>%
+    magrittr::extract(., j = "Feature" := match(get("Index"), Mapping_DT$Index) %>%
+                        magrittr::extract(Mapping_DT$Feature, .), with = FALSE) %T>%
     magrittr::extract(., j = "Abs_Weight" := abs(get("Weight"))) %T>%
     data.table::setorderv(., cols = c("Abs_Weight", "Feature"), order = c(-1, +1)) %>%
     magrittr::extract(., j = c("Feature", "Weight"), with = FALSE) %>%
